3c89aaa891fb605f91b8988feac595364a9dc53c
HenryYoshida7 <129332513+HenryYoshida7@users.noreply.github.com>
2023-07-03 18:34:01 -0300

    Add files via upload

---
+++ visualizar-hora com imagem/script.js
@@ -0,0 +1,14 @@
+function carregar() {
+    var msg = window.document.getElementById('msg');
+    var img = window.document.getElementById('imagem');
+    var data = new Date();
+    var hora = data.getHours();
+    msg.innerHTML = 'Agora são ' + hora + ' horas.';
+    if (hora >= 0 && hora < 12) {
+        img.src = 'img/ft-manha.jpg';
+    } else if (hora >= 12 && hora < 18) {
+        img.src = 'img/ft-tarde.jpg';
+    } else {
+        img.src = 'img/ft-noite.jpg';
+    }
+}
